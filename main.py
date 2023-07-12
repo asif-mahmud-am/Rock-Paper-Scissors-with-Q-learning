a26fd28ff457f7f02e614f54d17639d4b22d646f
Humairajahan <humairaazim45@gmail.com>
2023-07-12 16:56:12 +0600

    prettified

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ app = FastAPI()
 
 state = (0, 0)
 
+
 @app.post("/play")
 async def play(player_action: int):
     global state
     if player_action not in [0, 1, 2]:
-        return JSONResponse({"error": "Invalid move! Please enter 0, 1, or 2."}, status_code=400)
-    
+        return JSONResponse(
+            {"error": "Invalid move! Please enter 0, 1, or 2."}, status_code=400
+        )
+
     opponent_action = rps.get_action(state)
     if player_action == opponent_action:
         result = "It's a tie!"
         reward = 0
-    elif (player_action == 0 and opponent_action == 1) or (player_action == 1 and opponent_action == 2) or (
-            player_action == 2 and opponent_action == 0):
+    elif (
+        (player_action == 0 and opponent_action == 1)
+        or (player_action == 1 and opponent_action == 2)
+        or (player_action == 2 and opponent_action == 0)
+    ):
         result = "You lose!"
         reward = -1
     else:
@@ -29,9 +35,5 @@ async def play(player_action: int):
     rps.update_q_table(state, player_action, reward, next_state)
     state = next_state
 
-    response = {
-        "result": result,
-        "reward": reward,
-        "state": state
-    }
+    response = {"result": result, "reward": reward, "state": state}
     return response
